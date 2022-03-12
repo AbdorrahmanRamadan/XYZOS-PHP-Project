5f27ad554aca9a7b91ef8ee40de66aaf59a9f92c
unknown <fatmaabdelhameed1999@gmail.com>
2022-03-12 09:02:41 +0200

    views are added

---
+++ views/download_area.php
@@ -0,0 +1,44 @@
+<?php
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Download Data</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="src/css/style.css">
+</head>
+<body>
+<nav class="navbar">
+    <div class="container">
+        <div class="page_logo">
+            <img src="src/images/logo.png" class="logo" draggable="false">
+        </div>
+        <div class="profile">
+            <i class="fa-solid fa-circle-user profile-logo"></i>
+            <span class="username"><a href="">Fatma@gmail.com</a></span>
+        </div>
+    </div>
+</nav>
+<div class="download_body">
+    <div class="container">
+        <div class="download-data-image">
+            <img src="src/images/download.svg" class="download" draggable="false">
+        </div>
+        <div class="file-data">
+            <label>File Size: </label>
+            <span>350 MB</span>
+        </div>
+        <div class="download-link">
+            <div class="download-here">
+                Download From Here
+            </div>
+            <div class="link">
+                <a href="">https://www.bing.com/search?q=vectors&cvid=ac1a2acee5a7</a>
+            </div>
+        </div>
+    </div>
+
+</div>
+</body>
+</html>
---
+++ views/payment.php
@@ -0,0 +1,55 @@
+<?php
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>XYZ Product</title>
+        <link rel="stylesheet" href="src/css/style.css">
+    </head>
+    <body>
+        <img src="src/images/header_img.svg" class="header_img" draggable="false">
+        <nav class="navbar">
+            <div class="container">
+                <div class="page_logo">
+                    <img src="src/images/logo.png" class="logo" draggable="false">
+                </div>
+            </div>
+        </nav>
+        <div class="payment_body">
+            <div class="container">
+                <form class="payment_form" action="" method="post">
+                    <h3 class="form_header">Apply Your Payment</h3>
+                    <div class="input_container">
+                        <input type="email" name="email" placeholder="Type Email" class="input_field" value="">
+                        <label class="error_msg"></label>
+                    </div>
+                    <div class="input_container">
+                        <input type="password" name="password" placeholder="Type Password" class="input_field" value="">
+                        <label class="error_msg"></label>
+                    </div>
+                    <div class="input_container">
+                        <input type="password" name="confirm_pass" placeholder="Confirm Password" class="input_field" value="">
+                        <label class="error_msg"></label>
+                    </div>
+                    <div class="input_container">
+                        <input type="number" name="credit" placeholder="Type Credit Card Number" class="input_field" value="">
+                        <label class="error_msg"></label>
+                    </div>
+                    <div class="input_container">
+                        <input type="date" name="expire_date" placeholder="Type Expire Date" class="input_field" value="">
+                        <label class="error_msg"></label>
+                    </div>
+                    <div class="input_container">
+                        <input type="submit" name="register" class="btn_field" value="Submit">
+                    </div>
+                </form>
+                <div class="member_login">
+                    <label>If you are member</label>
+                    <a href="">Login</a>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
