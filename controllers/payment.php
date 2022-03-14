33ea69d282d955397182c9caeaf21383dbacb447
passanthamdy <passant.hamdy99@gmail.com>
2022-03-14 16:58:27 +0200

    payment page v1

--- controllers/payment.php
+++ controllers/payment.php
@@ -1,1 +1,47 @@
 <?php
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+function validate_payment_data($email, $password1, $password2, $creditcard, $expdate)
+{
+    $number = preg_match('@[0-9]@', $password1);
+    $uppercase = preg_match('@[A-Z]@', $password1);
+    $lowercase = preg_match('@[a-z]@', $password1);
+    $specialChars = preg_match('@[^\w]@', $password1);
+    $number_cc = preg_match('@[0-9]@', $creditcard);
+    $errors = array();
+    //email validation 
+    if (empty($email)) {
+        $errors += ["email" => "email is required"];
+    } else {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors += ["email" => "email is not valid"];
+        }
+    }
+
+    //password validation
+
+    if (strlen($password1) < 8 || !$number || !$uppercase || !$lowercase || !$specialChars) {
+        $errors += ["password1" => "Password must be at least 8 characters in length and must contain at least one number, 
+    one upper case letter, one lower case letter and one special character. \n"];
+    }
+    //confirm password validation
+    if (empty($password2)) {
+        $errors += ["password2" => "Password confirmation is required"];
+    } else {
+        if (!empty($password2) && $password1 != $password2) {
+            $errors += ["password2" => "Passswords didn't match"];
+        }
+    }
+    //credit card validation
+    if (strlen($creditcard) != 16 || !$number_cc) {
+        $errors += ["credit" => "Please enter a valid credit card number"];
+    }
+    //expiration date validation
+    if (empty($expdate)) {
+        $errors += ["expdate" => "credit card expiration is required"];
+    }
+    $errorJSON = json_encode($errors);
+    return $errorJSON;
+}
--- views/payment.php
+++ views/payment.php
@@ -1,55 +1,99 @@
 <?php
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+$erJSON = "";
+$userId;
+$result = array(
+    "email" => "",
+    "password1" => "",
+    "password2" => "",
+    "credit" => "",
+    "expdate" => ""
+);
+if (isset($_POST['validate'])) {
+    $email = $_POST['email'];
+    $password1 = $_POST['password1'];
+    $password2 = $_POST['password2'];
+    $creditcard = $_POST['creditcard'];
+    $expdate = $_POST['expire_date'];
+    require_once("../controllers/payment.php");
+    $erJSON = validate_payment_data($email, $password1, $password2, $creditcard, $expdate);
+    $result = json_decode($erJSON, true);
+    if (empty($result)){
+        require_once('../models/User.php');
+      $user = new User;
+      $user->insertUser($email,$password1);
+      $userId=$user->getUserByEmail($email);
+      require_once('../models/Order.php');
+      $order= new Order;
+      $key0= uniqid(time().'-key',TRUE);
+//$uid,$download_count, $product_link, $product_id
+      $order->insertOrder($userId,0,$key0, 1);
+
+
+    }
+}
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>XYZ Product</title>
-        <link rel="stylesheet" href="../src/css/style.css">
-    </head>
-    <body>
-        <img src="../src/images/header_img.svg" class="header_img" draggable="false">
-        <nav class="navbar">
-            <div class="container">
-                <div class="page_logo">
-                    <img src="../src/images/logo.png" class="logo" draggable="false">
-                </div>
-            </div>
-        </nav>
-        <div class="payment_body">
-            <div class="container">
-                <form class="payment_form" action="" method="post">
-                    <h3 class="form_header">Apply Your Payment</h3>
-                    <div class="input_container">
-                        <input type="email" name="email" placeholder="Type Email" class="input_field" value="">
-                        <label class="error_msg"></label>
-                    </div>
-                    <div class="input_container">
-                        <input type="password" name="password" placeholder="Type Password" class="input_field" value="">
-                        <label class="error_msg"></label>
-                    </div>
-                    <div class="input_container">
-                        <input type="password" name="confirm_pass" placeholder="Confirm Password" class="input_field" value="">
-                        <label class="error_msg"></label>
-                    </div>
-                    <div class="input_container">
-                        <input type="number" name="credit" placeholder="Type Credit Card Number" class="input_field" value="">
-                        <label class="error_msg"></label>
-                    </div>
-                    <div class="input_container">
-                        <input type="date" name="expire_date" placeholder="Type Expire Date" class="input_field" value="">
-                        <label class="error_msg"></label>
-                    </div>
-                    <div class="input_container">
-                        <input type="submit" name="register" class="btn_field" value="Submit">
-                    </div>
-                </form>
-                <div class="member_login">
-                    <label>If you are member</label>
-                    <a href="">Login</a>
-                </div>
-            </div>
 
+<head>
+    <meta charset="UTF-8">
+    <title>XYZ Product</title>
+    <link rel="stylesheet" href="../src/css/style.css">
+</head>
+
+<body>
+    <img src="../src/images/header_img.svg" class="header_img" draggable="false">
+    <nav class="navbar">
+        <div class="container">
+            <div class="page_logo">
+                <img src="../src/images/logo.png" class="logo" draggable="false">
+            </div>
         </div>
-    </body>
+    </nav>
+    <div class="payment_body">
+        <div class="container">
+            <form class="payment_form" action="" method="post">
+                <h3 class="form_header">Apply Your Payment</h3>
+                <div class="input_container">
+                    <input type="email" name="email" placeholder="Type Email" class="input_field" value="">
+                    <label class="error_msg"><?php if (isset($result['email'])) echo $result['email'];
+                                                else echo "";  ?></label>
+                </div>
+                <div class="input_container">
+                    <input type="password" name="password1" placeholder="Type Password" class="input_field" value="">
+                    <label class="error_msg"><?php if (isset($result['password1'])) echo $result['password1'];
+                                                else echo ""; ?></label>
+                </div>
+                <div class="input_container">
+                    <input type="password" name="password2" placeholder="Confirm Password" class="input_field" value="">
+                    <label class="error_msg"><?php if (isset($result['password2'])) echo $result['password2'];
+                                                else echo ""; ?></label>
+                </div>
+                <div class="input_container">
+                    <input type="number" name="creditcard" placeholder="Type Credit Card Number" class="input_field" value="">
+                    <label class="error_msg"><?php if (isset($result['credit'])) echo $result['credit'];
+                                                else echo ""; ?></label>
+                </div>
+                <div class="input_container">
+                    <input type="date" name="expire_date" placeholder="Type Expire Date" class="input_field" value="">
+                    <label class="error_msg"><?php if (isset($result['expdate'])) echo $result['expdate'];
+                                                else echo ""; ?></label>
+                </div>
+                <div class="input_container">
+                    <input type="submit" name="validate" class="btn_field" value="Submit">
+                </div>
+            </form>
+            <div class="member_login">
+                <label>If you are member</label>
+                <a href="">Login</a>
+                <?php echo $userId;?>
+            </div>
+        </div>
+
+    </div>
+</body>
+
 </html>
